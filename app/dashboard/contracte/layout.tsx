07e4233bb1f1641a54a7192a8ab4745c983c77ca
Code-W-A <radu.p1995@yahoo.com>
2026-03-16 09:56:03 +0200

    asdsa

--- app/dashboard/contracte/layout.tsx
+++ app/dashboard/contracte/layout.tsx
@@ -2,5 +2,5 @@ import type React from "react"
 import { ProtectedRoute } from "@/components/protected-route"
 
 export default function ContracteLayout({ children }: { children: React.ReactNode }) {
-  return <ProtectedRoute allowedRoles={["admin"]}>{children}</ProtectedRoute>
+  return <ProtectedRoute allowedRoles={["admin", "dispecer"]}>{children}</ProtectedRoute>
 }
